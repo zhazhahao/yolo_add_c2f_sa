12614d4b7c37b988fbb97dd40a81988e9ebc4940
zhazhahao <90495575+zhazhahao@users.noreply.github.com>
2024-01-19 16:38:22 +0800

    Add files via upload

---
+++ attention.py
@@ -0,0 +1,21 @@
+import torch
+from torch import nn
+
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x_true = x
+        avg_out = torch.mean(x, dim=1, keepdim=True)  # 平均池化
+        max_out, _ = torch.max(x, dim=1, keepdim=True)  # 最大池化
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return x_true*self.sigmoid(x) # 必须乘以x的值否则报错channel不对
